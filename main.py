4f076dd349d262b95822d832105aa4d7b1ef219d
kosta12341412 <potokin1008@gmail.com>
2025-02-15 18:41:07 +0300

    fafafaf

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import telebot
+
+API_TOKEN = "8070197745:AAElw3ZG6x-0VeKy_i_iL3intGV6qs5Fzi4"
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(commands=['register'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, f'{message.chat.id}')
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
